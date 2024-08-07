f223071fa41a81eedb11201f9eed4749e75e655d
Raktim Biswas <raktimb181@gmail.com>
2024-08-07 14:56:55 +0530

    namespace

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,17 @@
-#include<iostream>
-int main(){
-    std::cout << "hello Wrold" <<std::endl;
-    std::cout << "hii" <<std::endl;
-    
+#include <iostream>
+namespace first
+{
+    int x = 1;
+}
+namespace second
+{
+    int x = 2;
+}
+int main()
+{
+    const double PI = 3.14159;
+    double radius = 5.0;
+    double area = PI * radius * radius;
+    std::cout << "The area of the circle is " << area << std::endl;
     return 0;
 }
