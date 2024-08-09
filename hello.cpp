881cc7fefcd0d5878855a549afb0ac214f90a1dc
Raktim Biswas <raktimb181@gmail.com>
2024-08-09 15:13:00 +0530

    calculator

--- hello.cpp
+++ hello.cpp
@@ -1,62 +1,45 @@
 #include <iostream>
-#include <string>
-#include <cmath>
-// #include <vector>
-// typedef std::string text_t;
-// using text_t = std::string;
-int main()
-{
-    // using std ::cout;
-    // using std ::string;
-    // string name = " Raktim";
-    // cout << "hello" << name;
-    // text_t name = "raktim";
-    // std::cout << name;
-    // return 0;
-    // int students = 10;
-    // students+=1;
-    // std::cout << students;
-    // double x =  3.1415;
-    // int correct = 8;
-    // int questions =10;
-    // double score = correct/(double)questions * 100;
-    // std::cout << score <<"%";
-    // another
-    // std::string name;
-    // std::cout << "What's your full name ?:";
-    // std::getline(std::cin >>std::ws,name);
-    // int age;
-    // std::cout << "What is your age:";
-    // std::cin >> age;
-    // std::cin.ignore();
-    // std::cout << "Hello " << name << '\n';
-    // std::cout << "you are " << age << " years old";
-    // Hypotenuse
-    // double a, b, c;
-    // std::cout << "Enter the first number:";
-    // std::cin >> a;
-    // std::cout << "Enter the second number";
-    // std::cin >> b;
-    // c = sqrt(pow(a, 2) + pow(b, 2));
-    // std::cout << "Your answer is:" << c;
-    // switches
-    int month;
-    std::cout<< "Enter a number between 1-12:-";
-    std::cin >> month;
-    switch (month)
-    {
-    case 1:
-        std::cout << "It's january";
-        break;
-    case 2:
-        std::cout << "It's february";
-        break;
-    case 3:
-        std::cout << "It's something";
-        break;
-    
-    default:
-        std::cout << "It's not a month";
-        break;
+
+int main() {
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << "**************************** Calculator ****************************\n";
+    std::cout << "Enter either (+,-,*,/): ";
+    std::cin >> op;
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            std::cout << "Result: " << result;
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "Result: " << result;
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "Result: " << result;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                std::cout << "Result: " << result;
+            } else {
+                std::cout << "Error: Division by zero!";
+            }
+            break;
+        default:
+            std::cout << "Something went wrong!";
+            break;
     }
+
+    std::cout << "\n********************************************************************";
+    return 0;
 }
