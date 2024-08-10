1c5c4f730809a090b4eb4af8cb3eec1b37e34590
Raktim Biswas <raktimb181@gmail.com>
2024-08-10 14:59:40 +0530

    ternary operator

--- hello.cpp
+++ hello.cpp
@@ -1,45 +1,6 @@
 #include <iostream>
-
 int main() {
-    char op;
-    double num1;
-    double num2;
-    double result;
-
-    std::cout << "**************************** Calculator ****************************\n";
-    std::cout << "Enter either (+,-,*,/): ";
-    std::cin >> op;
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
-
-    switch (op) {
-        case '+':
-            result = num1 + num2;
-            std::cout << "Result: " << result;
-            break;
-        case '-':
-            result = num1 - num2;
-            std::cout << "Result: " << result;
-            break;
-        case '*':
-            result = num1 * num2;
-            std::cout << "Result: " << result;
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-                std::cout << "Result: " << result;
-            } else {
-                std::cout << "Error: Division by zero!";
-            }
-            break;
-        default:
-            std::cout << "Something went wrong!";
-            break;
-    }
-
-    std::cout << "\n********************************************************************";
+    int grade = 45;
+    grade>=60 ? std::cout << "you are pass!" : std::cout << "you are failed!";
     return 0;
 }
