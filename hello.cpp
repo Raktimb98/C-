3bc332034c850b44c23559a6be3eebcdda9b3c82
Raktim Biswas <raktimb181@gmail.com>
2024-08-11 09:59:55 +0530

    temperature calculator

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+
 int main() {
-    int grade = 45;
-    grade>=60 ? std::cout << "you are pass!" : std::cout << "you are failed!";
+    double temp;
+    char unit;
+    double result;
+
+    std::cout << "Enter the unit (c for Celsius, f for Fahrenheit): ";
+    std::cin >> unit;
+
+    std::cout << "Enter the temperature: ";
+    std::cin >> temp;
+
+    if (unit == 'c') {
+        result = (9.0 / 5) * temp + 32;
+        std::cout << "The temperature in Fahrenheit is: " << result << std::endl;
+    } else if (unit == 'f') {
+        result = (5.0 / 9) * (temp - 32);
+        std::cout << "The temperature in Celsius is: " << result << std::endl;
+    } else {
+        std::cout << "Invalid unit entered." << std::endl;
+    }
+
     return 0;
 }
