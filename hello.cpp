2f32e6402eaaceb5bd8ad3761154a3a21acd1bb2
Raktim Biswas <raktimb181@gmail.com>
2024-08-12 10:55:34 +0530

    While loop

--- hello.cpp
+++ hello.cpp
@@ -2,10 +2,11 @@
 #include <string>
 int main() {
     std::string name;
-    std::cout << "Enter your name:";
-    std::getline(std::cin,name);
-    if(name.length()>12){
-        std::cout << "Your name is too long.";
+    while (name.empty())
+    {
+        std::cout<<"Enter your name:";
+        std::getline(std::cin,name);
     }
+        std::cout << "Hello " << name;
     return 0;
 }
