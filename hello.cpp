242a46ab300d50733036c326d5098592baae5c04
Raktim Biswas <raktimb181@gmail.com>
2024-08-12 10:50:18 +0530

    string functions

--- hello.cpp
+++ hello.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
-
+#include <string>
 int main() {
-    double temp;
-    char unit;
-    double result;
-
-    std::cout << "Enter the unit (c for Celsius, f for Fahrenheit): ";
-    std::cin >> unit;
-
-    std::cout << "Enter the temperature: ";
-    std::cin >> temp;
-
-    if (unit == 'c') {
-        result = (9.0 / 5) * temp + 32;
-        std::cout << "The temperature in Fahrenheit is: " << result << std::endl;
-    } else if (unit == 'f') {
-        result = (5.0 / 9) * (temp - 32);
-        std::cout << "The temperature in Celsius is: " << result << std::endl;
-    } else {
-        std::cout << "Invalid unit entered." << std::endl;
+    std::string name;
+    std::cout << "Enter your name:";
+    std::getline(std::cin,name);
+    if(name.length()>12){
+        std::cout << "Your name is too long.";
     }
-
     return 0;
 }
