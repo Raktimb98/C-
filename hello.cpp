50610602bb6ad722497e1264c1a92949308dea87
Raktim Biswas <raktimb181@gmail.com>
2024-08-15 10:22:52 +0530

    Concatination

--- hello.cpp
+++ hello.cpp
@@ -1,25 +1,12 @@
 #include <iostream>
-
-int main() {
-    double temp;
-    char unit;
-    double result;
-
-    std::cout << "Enter the unit (c for Celsius, f for Fahrenheit): ";
-    std::cin >> unit;
-
-    std::cout << "Enter the temperature: ";
-    std::cin >> temp;
-
-    if (unit == 'c') {
-        result = (9.0 / 5) * temp + 32;
-        std::cout << "The temperature in Fahrenheit is: " << result << std::endl;
-    } else if (unit == 'f') {
-        result = (5.0 / 9) * (temp - 32);
-        std::cout << "The temperature in Celsius is: " << result << std::endl;
-    } else {
-        std::cout << "Invalid unit entered." << std::endl;
-    }
-
+std::string concatString(std::string firstname, std::string lastname);
+int main(){
+    std::string firstname = "Raktim";
+    std::string lastname = "Biswas";
+    std::string full_name = concatString(firstname, lastname);
+    std::cout << full_name;
     return 0;
+}
+std::string concatString(std::string firstname, std::string lastname){
+    return firstname + " " + lastname;
 }
