7fe7f1cd5461f14718e321ab7c1220b97d96eff7
Raktim Biswas <raktimb181@gmail.com>
2024-08-28 12:57:11 +0530

    Iterate over an array

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
-int main(){
-    std::string food[] ={"Egg chicken roll" , "Paneer tikka" ,"Garlic naan"};
-    std::cout<<sizeof(food)/sizeof(std::string)<<" items";
-    return 0;
-}
+// #include<iostream>
+// int main(){
+//     std::string food[] ={"Egg chicken roll" , "Paneer tikka" ,"Garlic naan"};
+//     std::cout<<sizeof(food)/sizeof(std::string)<<" items";
+//     return 0;
+// }
+// Iterate over an array
+// #include <iostream>
+// int main()
+// {
+//     std::string food[] = {"Egg chicken roll", "Paneer tikka", "Garlic naan" , "Fried rice"};
+//     for (int i = 0; i < sizeof(food) / sizeof(std::string); i++)
+//     {
+//         std::cout << food[i] << '\n';
+//     }
+
+//     return 0;
+// }
