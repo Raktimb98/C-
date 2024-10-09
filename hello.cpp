1fd80e9dd6db50419148bc21214366f5101b6db2
Raktim Biswas <raktimb181@gmail.com>
2024-10-09 18:43:28 +0530

    Search an array for an element

--- hello.cpp
+++ hello.cpp
@@ -25,22 +25,53 @@
 //     }
 // }
 // Pass array to a function
+// #include <iostream>
+// double getTotal(double prices[], int size);
+// int main()
+// {
+//     double prices[] = {8.9, 40, 48.6, 543.4, 54.8};
+//     int size = sizeof(prices) / sizeof(double);
+//     double total = getTotal(prices, size);
+//     std::cout << total;
+//     return 0;
+// }
+// double getTotal(double prices[], int size)
+// {
+//     double total = 0;
+//     for (int i = 0; i < size; i++)
+//     {
+//         total += prices[i];
+//     }
+//     return total;
+// }
+//Search an array for an element
 #include <iostream>
-double getTotal(double prices[], int size);
-int main()
-{
-    double prices[] = {8.9, 40, 48.6, 543.4, 54.8};
-    int size = sizeof(prices) / sizeof(double);
-    double total = getTotal(prices, size);
-    std::cout << total;
+int searchArray(int array[],int size,int element);
+int main(){
+    int numbers[] = {1, 2, 3, 4, 5,6,7,8,9,10};
+    int size = sizeof(numbers)/sizeof(numbers[0]);
+    int index;
+    int myNum;
+
+    std::cout <<"Enter the search element:" <<'\n';
+    std::cin >> myNum;
+    index = searchArray(numbers, size, myNum);
+    if(index != -1){
+        std::cout<< myNum <<" is at index " << index;
+    }
+    else{
+        std::cout<< myNum <<" is not in array";
+    }
     return 0;
 }
-double getTotal(double prices[], int size)
-{
-    double total = 0;
+int searchArray(int array[],int size,int element){
     for (int i = 0; i < size; i++)
     {
-        total += prices[i];
+        if (array[i] == element)
+        {
+            return i;
+        }
+        
     }
-    return total;
+    return -1;
 }
