c0376e02393da6c375292e9a5791faeca69848bf
Raktim Biswas <raktimb181@gmail.com>
2024-10-15 07:33:19 +0530

    Fill function

--- hello.cpp
+++ hello.cpp
@@ -44,7 +44,7 @@
 //     }
 //     return total;
 // }
-//Search an array for an element
+// Search an array for an element
 // #include <iostream>
 // int searchArray(int array[],int size,int element);
 // int main(){
@@ -71,35 +71,52 @@
 //         {
 //             return i;
 //         }
-        
+
 //     }
 //     return -1;
 // }
-//Sort an array
+// Sort an array
+// #include <iostream>
+// void sort(int array[],int size);
+// int main(){
+//     int array[] = {64, 34, 25, 12, 22, 10 , 1 , 6};
+//     int size = sizeof(array)/sizeof(array[0]);
+//     sort(array, size);
+//     for (int element : array)
+//     {
+//         std::cout << element << " ";
+//     }
+
+//     return 0;
+// }
+// void sort(int array[],int size){
+//     int temp;
+//     for (int i = 0; i < size -1 ;i++){
+//         for (int j = 0; j < size - i - 1; j++){
+//             if (array[j] > array[j+1])
+//             {
+//                 temp = array[j];
+//                 array[j] = array[j+1];
+//                 array[j+1] = temp;
+//             }
+
+//     }
+// }
+// }
+
+// Fill() function
 #include <iostream>
-void sort(int array[],int size);
-int main(){
-    int array[] = {64, 34, 25, 12, 22, 10 , 1 , 6};
-    int size = sizeof(array)/sizeof(array[0]);
-    sort(array, size);
-    for (int element : array)
+int main()
+{
+    const int size = 150;
+    std::string foods[size];
+    fill(foods, foods + (size / 2), "Raktim");
+    fill(foods + (size / 2), foods + size, "Biswas");
+
+    for (std::string items : foods)
     {
-        std::cout << element << " ";
+        std::cout << items << '\n';
     }
-    
+
     return 0;
-}
-void sort(int array[],int size){
-    int temp;
-    for (int i = 0; i < size -1 ;i++){
-        for (int j = 0; j < size - i - 1; j++){
-            if (array[j] > array[j+1])
-            {
-                temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-            
-    }
-}
 }
